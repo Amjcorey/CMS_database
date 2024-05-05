4d0162c6f65fcdf916bb56d372f1e5b1a92b1bd4
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-04 21:23:20 -0700

    update

---
+++ server.js
@@ -0,0 +1,12 @@
+const inquier = require('inquirer');
+const mysql = require('mysql12');
+
+
+const db = mysql.createConnection (
+    [
+        host: '127.0.0.1',
+        user: 'root',
+        
+
+    ]
+)
